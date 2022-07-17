fe8cb3daed3506a59bbf7e94f9e6ad48b800c0fa
jithin <jithinp69@gmail.com>
2022-07-17 12:43:08 +0530

    WIP: Backend created -Hello world!

---
+++ src/backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/hello',(req,res)=>{
+    res.send("Hello World!");
+})
+
+
+app.listen(3005, ()=>{
+    console.log("Server started @ 3005");
+});
