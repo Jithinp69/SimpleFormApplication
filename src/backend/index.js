f5f61fcc1102863a4e9e490a7ef72be96c8139af
jithin <jithinp69@gmail.com>
2022-07-17 16:24:58 +0530

    Backend updated

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
+
+app.use(express.json())
+
 app.get('/hello',(req,res)=>{
     res.send("Hello World!");
-})
+});
 
+app.post('/student',(req,res)=>{
+    console.log(req.body);
+    const data = JSON.stringify(req.body);
+    fs.appendFile('student.txt', data, (err)=>{
+        if(err){
+            console.log("Error Occured");
+        }else{
+            res.send("Created a file and saved")
+        }
+    })
+    res.send('Done');
+});
 
 app.listen(3005, ()=>{
     console.log("Server started @ 3005");
